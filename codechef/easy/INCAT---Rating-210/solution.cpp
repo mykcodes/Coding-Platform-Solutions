f06df06e014471646dd9d9ff152c52fc7dd0269a
Mayank Sharma <ffmayank55@gmail.com>
2026-08-21 22:52:14 +0530

    ✅ INCAT - Rating 210 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-21

---
+++ codechef/easy/INCAT---Rating-210/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s,X;
+    cin>>s;
+    X="act";
+    sort(s.begin(),s.end());
+    (s==X)
+    ? cout<<"Yes"
+    : cout<<"No";
+    
+    return 0;
+}
