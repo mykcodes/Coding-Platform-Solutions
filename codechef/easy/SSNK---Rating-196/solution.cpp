c964c718f74292fc37ec45cd0534ed5b49a4005c
Mayank Sharma <ffmayank55@gmail.com>
2026-08-23 23:01:13 +0530

    ✅ SSNK - Rating 196 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/easy/SSNK---Rating-196/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string S;
+	cin>>S;
+	if (S.front()=='s' || S.back()=='s') {
+	    cout<<"Yes";
+	} else {
+	    cout<<"No";
+	}
+	return 0;
+
+}
