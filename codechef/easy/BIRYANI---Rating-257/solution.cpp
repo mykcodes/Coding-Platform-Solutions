0708acd03d874220492311249c557cf69eec3e79
Mayank Sharma <ffmayank55@gmail.com>
2026-08-31 22:42:39 +0530

    ✅ BIRYANI - Rating 257 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/easy/BIRYANI---Rating-257/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T>0) {
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x*y<<endl;
+	    T--;
+	}
+    return 0;
+}
