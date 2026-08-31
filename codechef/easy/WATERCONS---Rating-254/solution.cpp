089068be550110f5d31fb2f489795b63bb58deee
Mayank Sharma <ffmayank55@gmail.com>
2026-08-31 22:27:12 +0530

    ✅ WATERCONS - Rating 254 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/easy/WATERCONS---Rating-254/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if (x>=2000) {
+            cout<<"Yes\n";
+        } else {
+            cout<<"No\n";
+        }
+    }
+	return 0;
+}
