f9133f995cd81a3fbc0a280f5cbe75a82d12170a
Mayank Sharma <ffmayank55@gmail.com>
2026-08-31 22:36:34 +0530

    ✅ TOP10 - Rating 255 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/easy/TOP10---Rating-255/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--) {
+	    int x;
+	    cin>>x;
+	    if (x<=10) {
+	        cout<<"Yes\n";
+        } else {
+            cout<<"No\n";
+        }
+	}
+	return 0;
+
+}
