02fb33b347278c4ce0005716ce2ab87f5586e812
Mayank Sharma <ffmayank55@gmail.com>
2026-09-01 20:17:06 +0530

    ✅ WEIGHTLIFT - Rating 270 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/easy/WEIGHTLIFT---Rating-270/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A1,B1,A2,B2,C1,C2;
+	cin>>A1>>A1>>B1>>B2>>C1>>C2;
+    cout<<max(A1, A2)+ max(B1, B2)+ max(C1, C2);
+    return 0;
+}
