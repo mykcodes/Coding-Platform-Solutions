03c846c295b0ab6f2a750af6e1ce054cb75837b5
Mayank Sharma <ffmayank55@gmail.com>
2026-09-03 23:12:27 +0530

    ✅ TAXES - Rating 276 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-03

---
+++ codechef/easy/TAXES---Rating-276/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int X;
+	cin>>X;
+	if (X>100) {
+	    cout<<X-10;
+	} else {
+	    cout<<"Yo 🥀"
+	}
+    }
+	
+
+}
