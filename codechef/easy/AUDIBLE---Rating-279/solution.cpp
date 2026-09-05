b23996c7bf53cdcbe8c514b760965f3bea21075c
Mayank Sharma <ffmayank55@gmail.com>
2026-09-05 16:52:00 +0530

    ✅ AUDIBLE - Rating 279 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/easy/AUDIBLE---Rating-279/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int  t;
+	cin>>t;
+	while (t--) {
+	    int x;
+	    cin>>x;
+	    if (x>=67 && x<=45000) {
+	        cout<<"Yes"<<endl;
+	    } else {
+	        cout<<"No"<<endl;
+	    }
+	}
+
+}
