bf5371d364a05ce713ae329b0035d945f6aa95a6
Mayank Sharma <ffmayank55@gmail.com>
2026-09-07 20:02:09 +0530

    ✅ PRACTICEPERF - Rating 467 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/easy/PRACTICEPERF---Rating-467/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int arr[4];
+	int count=0;
+	for (int i=0; i < 4; i ++) {
+	    cin>>arr[i];
+	}
+	for (int i=0; i < 4; i ++) {
+	    if (arr[i]>=10) {
+	        count++;
+	    }
+	}
+	cout<<count;
+	return 0;
+
+}
